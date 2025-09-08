f8af5b16050635d07f6712e8d04642f2123fc99b
HollieMorrison <104519409+HollieMorrison@users.noreply.github.com>
2025-09-09 00:24:40 +0100

    imports for typer and rich added to app.py file

---
+++ app.py
@@ -0,0 +1,9 @@
+# Main entry for my aromavault CLI application and also uses Typer to manage commands, and Rich to make outputs look better.
+
+from typing import Optional, List
+import sys
+import typer
+from rich.console import Console
+from rich.table import Table
+
+
